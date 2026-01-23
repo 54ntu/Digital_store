0b46885f1c3a0219f9cb8641a998d07361bfa670
54ntu <chaudharysantaram8@gmail.com>
2026-01-23 01:58:15 +0000

    user model is defined

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize"
+import { sequelize } from "../dbconfig/db.js"
+
+
+const User = sequelize.define(
+    "User",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+
+        username: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        role: {
+            type: DataTypes.ENUM("customer", "admin"),
+            defaultValue: "customer"
+        },
+        otp: DataTypes.STRING,
+        otpGeneratedTime: DataTypes.STRING,
+
+
+    },
+    {
+        tableName: "users",
+        timestamps: true,
+    }
+)
+
+
+export default User;
