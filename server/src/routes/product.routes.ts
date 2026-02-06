77030231d5e3603c3d42d961ddac34cb53fe70a3
54ntu <chaudharysantaram8@gmail.com>
2026-02-06 23:00:02 +0000

    product router is created

--- server/src/routes/product.routes.ts
+++ server/src/routes/product.routes.ts
@@ -5,6 +5,11 @@ import upload from "../middlewares/multer.middleware.js";
 const productRouter = express.Router();
 
 productRouter.route("/add-product").post(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, upload.single("productImage"), ProductController.createProduct);
+productRouter.route("/get-product").get(ProductController.getProduct)
+productRouter.route("/get-single").get(ProductController.getSingleProduct)
+productRouter.route("/update-product").patch(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, upload.single("productImage"), ProductController.updateProduct)
+productRouter.route("/update-stock").patch(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, ProductController.updateProductStock)
+productRouter.route("/delete-product").delete(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, ProductController.deleteProduct)
 
 
 
