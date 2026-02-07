31bb4205490c9d0a7a8d1f39a985c0a5e0adfa0f
54ntu <chaudharysantaram8@gmail.com>
2026-02-07 23:02:35 +0000

    errorhandler is implemented in user routes

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -11,216 +11,189 @@ import { hashValue } from "../handler/otpHash.js";
 
 class UserController {
     static async register(req: Request, res: Response) {
-        try {
 
-            const { username, email, password } = req.body
-            if (!username || !email || !password) {
-                return res.status(400).json({
-                    message: "username, email and password are required!!!!"
-                })
-            }
-
-            //check if user already exists or not
-
-            const data = await User.findOne({
-                where: {
-                    email: email
-                }
+        const { username, email, password } = req.body
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "username, email and password are required!!!!"
             })
-
-
-            if (data) {
-                return res.status(400).json({
-                    message: "user with the given email already exists  !!!!    "
-                })
-            }
-
-            const hashedPassword = await EncryptDecryptPassHandler.hashPassword(password)
-
-            const user = await User.create({
-                email,
-                password: hashedPassword,
-                username
-            })
-
-
-            if (user) {
-                res.status(201).json({
-                    message: "user registered successfully!!!!"
-                })
-            }
-
-            await sendMail({
-                to: email,
-                subject: "Welcome to Digital Dokan!",
-                text: `Congralution ${username}, you have been successfully registered on Digital Dokan. We are excited to have you on board!`
-            })
-
-            return res.status(500).json({
-                message: " something went wrong!!!! please try again later  "
-            })
-        } catch (error) {
-            return res.status(500).json({
-                message: "internal server error",
-                error: error
-            })
-
         }
+
+        //check if user already exists or not
+
+        const data = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+
+
+        if (data) {
+            return res.status(400).json({
+                message: "user with the given email already exists  !!!!    "
+            })
+        }
+
+        const hashedPassword = await EncryptDecryptPassHandler.hashPassword(password)
+
+        const user = await User.create({
+            email,
+            password: hashedPassword,
+            username
+        })
+
+
+        if (user) {
+            res.status(201).json({
+                message: "user registered successfully!!!!"
+            })
+        }
+
+        await sendMail({
+            to: email,
+            subject: "Welcome to Digital Dokan!",
+            text: `Congralution ${username}, you have been successfully registered on Digital Dokan. We are excited to have you on board!`
+        })
+
+        return res.status(500).json({
+            message: " something went wrong!!!! please try again later  "
+        })
+
 
     }
 
 
     static async login(req: Request, res: Response) {
-        try {
-            const { email, password } = req.body;
 
-            if (!email || !password) {
-                return res.status(400).json({
-                    message: "email or password is required!!!!!"
-                })
-            }
+        const { email, password } = req.body;
 
-            //if email or username and password is given then check for user exist or not
-            const [user] = await User.findAll({
-                where: {
-
-                    email: email
-
-                }
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "email or password is required!!!!!"
             })
+        }
 
-            // console.log("user : ", user)
-            if (!user) {
-                return res.status(404).json({
-                    message: "user with the given email does not exist!!!!!!"
-                })
-            }
+        //if email or username and password is given then check for user exist or not
+        const [user] = await User.findAll({
+            where: {
 
-            const isPasswordMatched = await EncryptDecryptPassHandler.comparePassword(password, user.password);
-            // console.log(isPasswordMatched)
-
-            if (!isPasswordMatched) {
-                return res.status(401).json({
-                    message: "invalid credentials!!!!!"
-                })
+                email: email
 
             }
+        })
 
-            const accessToken = jwt.sign(
-                {
-                    id: user.id,
-                    email: user.email,
-                    role: user.role
-                },
+        // console.log("user : ", user)
+        if (!user) {
+            return res.status(404).json({
+                message: "user with the given email does not exist!!!!!!"
+            })
+        }
 
-                envConfig.accessTokenSecret,
-                {
-                    expiresIn: envConfig.accessTokenExpiry
+        const isPasswordMatched = await EncryptDecryptPassHandler.comparePassword(password, user.password);
+        // console.log(isPasswordMatched)
 
-                }
-            )
-
-            // console.log("accessToken : ", accessToken)
-
-            //before we set token into the cookie we have to set some options
-            const cookieOptions = {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'strict' as const
-            }
-
-
-            return res.status(200)
-                .cookie('accessToken', accessToken, cookieOptions)
-                .json({
-                    message: "user logged in successfully!!!!!",
-                    accessToken: accessToken
-                })
-        } catch (error) {
-            return res.status(500).json({
-                message: "internal server error",
-                error: error
+        if (!isPasswordMatched) {
+            return res.status(401).json({
+                message: "invalid credentials!!!!!"
             })
 
         }
+
+        const accessToken = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                role: user.role
+            },
+
+            envConfig.accessTokenSecret,
+            {
+                expiresIn: envConfig.accessTokenExpiry
+
+            }
+        )
+
+        // console.log("accessToken : ", accessToken)
+
+        //before we set token into the cookie we have to set some options
+        const cookieOptions = {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict' as const
+        }
+
+
+        return res.status(200)
+            .cookie('accessToken', accessToken, cookieOptions)
+            .json({
+                message: "user logged in successfully!!!!!",
+                accessToken: accessToken
+            })
+
     }
 
 
     static async logout(req: Request, res: Response) {
-        try {
-            const options = {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'strict' as const,
-            }
 
-            return res.status(200)
-                .clearCookie('accessToken', options)
-                .json({
-                    message: "user logged out successfully!!!!!"
-                })
+        const options = {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict' as const,
+        }
 
-        } catch (error) {
-            return res.status(500).json({
-                message: "internal server error",
-                error: error
+        return res.status(200)
+            .clearCookie('accessToken', options)
+            .json({
+                message: "user logged out successfully!!!!!"
             })
 
-        }
+
     }
 
     static async forgotPassword(req: Request, res: Response) {
         const { email } = req.body;
 
-        try {
-            if (!email) {
-                return res.status(400).json({
-                    message: "email is required!!!!!"
-                })
-            }
-
-            const user = await User.findOne({
-                where: {
-                    email: email
-                }
+        if (!email) {
+            return res.status(400).json({
+                message: "email is required!!!!!"
             })
-
-            if (!user) {
-                return res.status(404).json({
-                    message: "user with the given email does not exist!!!!!!"
-                })
-            }
-
-            const otp = generateOtp()
-            const resetSessionId = uuid4();
-
-            await PasswordResetSession.create({
-                id: resetSessionId,
-                userId: user.id,
-                otpHash: hashValue(otp.toString()),
-                expiresAt: Date.now() + 10 * 60 * 1000 //10 minutes
-            })
-
-
-            await sendMail({
-                to: email,
-                subject: "Password Reset OTP - Digital Dokan",
-                text: `Your OTP for password reset is ${otp}. It is valid for 10 minutes. If you did not request a password reset, please ignore this email.`
-            })
-
-
-
-            return res.status(200).json({
-                message: "OTP has been sent to your email address!!!!!",
-                resetSessionId: resetSessionId
-            })
-        } catch (error) {
-            return res.status(500).json({
-                message: "internal server error",
-                error: error
-            })
-
         }
+
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user) {
+            return res.status(404).json({
+                message: "user with the given email does not exist!!!!!!"
+            })
+        }
+
+        const otp = generateOtp()
+        const resetSessionId = uuid4();
+
+        await PasswordResetSession.create({
+            id: resetSessionId,
+            userId: user.id,
+            otpHash: hashValue(otp.toString()),
+            expiresAt: Date.now() + 10 * 60 * 1000 //10 minutes
+        })
+
+
+        await sendMail({
+            to: email,
+            subject: "Password Reset OTP - Digital Dokan",
+            text: `Your OTP for password reset is ${otp}. It is valid for 10 minutes. If you did not request a password reset, please ignore this email.`
+        })
+
+
+
+        return res.status(200).json({
+            message: "OTP has been sent to your email address!!!!!",
+            resetSessionId: resetSessionId
+        })
 
 
     }
@@ -229,45 +202,37 @@ class UserController {
 
     static async verifyOtp(req: Request, res: Response) {
         const { otp, resetSessionId } = req.body;
-        try {
-            const session = await PasswordResetSession.findOne({
-                where: {
-                    id: resetSessionId
-                }
-            })
-
-            if (!session || session.expiresAt < Date.now()) {
-                return res.status(400).json({
-                    message: "OTP expired or invalid!!!!!"
-                })
+        const session = await PasswordResetSession.findOne({
+            where: {
+                id: resetSessionId
             }
+        })
 
-            if (session.attempts >= 5) {
-                return res.status(429).json({
-                    message: "Too many attempts."
-                })
-            }
-
-            if (hashValue(otp) !== session.otpHash) {
-                session.attempts += 1;
-                await session.save();
-                return res.status(400).json({
-                    message: "Invalid OTP!!!!!"
-                })
-            }
-
-            session.verified = true;
-            await session.save();
-            return res.status(200).json({
-                message: "OTP verified successfully!!!!!"
+        if (!session || session.expiresAt < Date.now()) {
+            return res.status(400).json({
+                message: "OTP expired or invalid!!!!!"
             })
-        } catch (error) {
-            return res.status(500).json({
-                message: "internal server error",
-                error: error
-            })
-
         }
+
+        if (session.attempts >= 5) {
+            return res.status(429).json({
+                message: "Too many attempts."
+            })
+        }
+
+        if (hashValue(otp) !== session.otpHash) {
+            session.attempts += 1;
+            await session.save();
+            return res.status(400).json({
+                message: "Invalid OTP!!!!!"
+            })
+        }
+
+        session.verified = true;
+        await session.save();
+        return res.status(200).json({
+            message: "OTP verified successfully!!!!!"
+        })
 
     }
 
