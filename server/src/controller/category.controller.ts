965ead61c94d4c4226dcbb7160578081004816a3
54ntu <chaudharysantaram8@gmail.com>
2026-02-07 23:05:31 +0000

    error handler function is implemented for category routes

--- server/src/controller/category.controller.ts
+++ server/src/controller/category.controller.ts
@@ -1,165 +1,138 @@
 import type { Request, Response } from "express";
 import { Category } from "../models/category.model.js";
-import { ApiError } from "../handler/errorHandler.js";
 
 class CategoryController {
     // Controller methods will be implemented here
     static async addCategory(req: Request, res: Response) {
 
-        try {
-
-            console.log(req.body)
-            // Implementation for adding a category
-            const { categoryName } = req.body;
+        // console.log(req.body)
+        // Implementation for adding a category
+        const { categoryName } = req.body;
 
 
-            if (!categoryName) {
-                return res.status(400).json({ message: "Category name is required" });
-            }
-
-            //check whether category already exists
-            const existingCategory = await Category.findAll({
-                where: {
-                    categoryName: categoryName
-                }
-            })
-
-            if (existingCategory.length > 0) {
-                return res.status(409).json({ message: "Category already exists" });
-            }
-
-            const newCategory = await Category.create({
-                categoryName: categoryName
-            })
-
-
-            if (!newCategory) {
-                return res.status(500).json({ message: "Failed to create category" });
-            }
-
-            return res.status(201).json({
-                message: "Category created successfully",
-                category: newCategory
-            })
-        } catch (error) {
-            return res.status(500).json({
-                message: "internal server error",
-                error: error
-            })
-
+        if (!categoryName) {
+            return res.status(400).json({ message: "Category name is required" });
         }
+
+        //check whether category already exists
+        const existingCategory = await Category.findAll({
+            where: {
+                categoryName: categoryName
+            }
+        })
+
+        if (existingCategory.length > 0) {
+            return res.status(409).json({ message: "Category already exists" });
+        }
+
+        const newCategory = await Category.create({
+            categoryName: categoryName
+        })
+
+
+        if (!newCategory) {
+            return res.status(500).json({ message: "Failed to create category" });
+        }
+
+        return res.status(201).json({
+            message: "Category created successfully",
+            category: newCategory
+        })
+
 
     }
 
     static async getAllCategories(req: Request, res: Response) {
-        try {
-            const categories = await Category.findAll();
-            if (categories.length === 0) {
-                return res.status(404).json({
-                    message: "No categories found"
-                })
-            }
-            return res.status(200).json({
-                message: "Categories retrieved successfully",
-                categories: categories
-            })
-        } catch (error) {
-            return res.status(500).json({
-                message: "internal server error",
-                error: error
-            })
 
+        const categories = await Category.findAll();
+        if (categories.length === 0) {
+            return res.status(404).json({
+                message: "No categories found"
+            })
         }
+        return res.status(200).json({
+            message: "Categories retrieved successfully",
+            categories: categories
+        })
+
     }
 
     static async updateCategory(req: Request, res: Response): Promise<void> {
-        try {
-            const { id } = req.params;
-            const { categoryName } = req.body;
-            if (!id || !categoryName) {
-                res.status(400).json({
-                    message: "category id and name are required!!!!"
-                })
-                return;
-            }
 
-            //check whether the category of given id exists or not
-            const existingCategory = await Category.findAll({
-                where: {
-                    id: id
-                }
+        const { id } = req.params;
+        const { categoryName } = req.body;
+        if (!id || !categoryName) {
+            res.status(400).json({
+                message: "category id and name are required!!!!"
             })
-
-
-
-            if (existingCategory.length === 0) {
-                res.status(404).json({
-                    message: "Category not found"
-                })
-                return;
-            }
-
-            //if found then update the existing category
-            const updatedCategory = await Category.update({
-                categoryName: categoryName
-
-            }, {
-                where: {
-                    id: id
-                }
-            })
-
-
-
-            res.status(200).json({
-                message: "category updated successfully",
-                updatedCategory: updatedCategory
-            })
-        } catch (error) {
-
-            throw new ApiError(500, "Internal server error")
-
-
+            return;
         }
+
+        //check whether the category of given id exists or not
+        const existingCategory = await Category.findAll({
+            where: {
+                id: id
+            }
+        })
+
+
+
+        if (existingCategory.length === 0) {
+            res.status(404).json({
+                message: "Category not found"
+            })
+            return;
+        }
+
+        //if found then update the existing category
+        const updatedCategory = await Category.update({
+            categoryName: categoryName
+
+        }, {
+            where: {
+                id: id
+            }
+        })
+
+
+
+        res.status(200).json({
+            message: "category updated successfully",
+            updatedCategory: updatedCategory
+        })
+
 
 
     }
 
     static async deleteCategory(req: Request, res: Response): Promise<void> {
-        try {
-            const { id } = req.params;
+        const { id } = req.params;
 
-            //first check whether category with given id exists or not
-            const existingCategory = await Category.findAll({
-                where: {
-                    id: id
-                }
-            })
-
-            if (existingCategory.length === 0) {
-                res.status(404).json({
-                    message: "Category not found"
-                })
-                return;
+        //first check whether category with given id exists or not
+        const existingCategory = await Category.findAll({
+            where: {
+                id: id
             }
+        })
 
-            //if found then delete the category
-            await Category.destroy({
-                where: {
-                    id: id
-                }
+        if (existingCategory.length === 0) {
+            res.status(404).json({
+                message: "Category not found"
             })
-
-            res.status(200).json({
-                message: "Category deleted successfully!!!!!!!!!!!!!!!!!!!"
-            })
-        } catch (error) {
-            res.status(500).json({
-                message: "internal server error",
-                error: error
-            })
-
+            return;
         }
+
+        //if found then delete the category
+        await Category.destroy({
+            where: {
+                id: id
+            }
+        })
+
+        res.status(200).json({
+            message: "Category deleted successfully!!!!!!!!!!!!!!!!!!!"
+        })
+
     }
 
 }
--- server/src/routes/category.routes.ts
+++ server/src/routes/category.routes.ts
@@ -1,12 +1,13 @@
 import express from "express"
 import CategoryController from "../controller/category.controller.js";
 import UserMiddleware from "../middlewares/auth.middleware.js";
+import errorHandler from "../handler/errorHandler.js";
 const categoryRouter = express.Router();
 
-categoryRouter.route("/add-category").post(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, CategoryController.addCategory);
-categoryRouter.route("/get-categories").get(CategoryController.getAllCategories);
-categoryRouter.route("/update-category/:id").patch(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, CategoryController.updateCategory);
-categoryRouter.route("/delete-category/:id").delete(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, CategoryController.deleteCategory);
+categoryRouter.route("/add-category").post(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, errorHandler(CategoryController.addCategory));
+categoryRouter.route("/get-categories").get(errorHandler(CategoryController.getAllCategories));
+categoryRouter.route("/update-category/:id").patch(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, errorHandler(CategoryController.updateCategory));
+categoryRouter.route("/delete-category/:id").delete(UserMiddleware.isUserLoggedIn, UserMiddleware.isAdmin, errorHandler(CategoryController.deleteCategory));
 
 
 export default categoryRouter;
