879dc24436b33af6306a58216a27c6b7ef47c370
54ntu <chaudharysantaram8@gmail.com>
2026-02-11 10:03:24 +0000

    working on orderController

---
+++ server/src/controller/order.controller.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from "express";
+
+class OrderController {
+    static async createOrder(req:Request,res:Response):Promise<void>{
+        
+    }
+
+}
+
+export default OrderController
