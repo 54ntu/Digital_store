4e9b1b32ee87b42cdb50542ef2b9dd4862f315bd
54ntu <chaudharysantaram8@gmail.com>
2026-02-13 00:40:08 +0000

    cart router is created

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import router from './routes/user.routes.js';
 import categoryRouter from './routes/category.routes.js';
 import productRouter from './routes/product.routes.js';
 import orderRouter from './routes/order.routes.js';
+import cartRouter from './routes/cart.routes.js';
 const app = express();
 
 app.use(express.json());
@@ -23,5 +24,9 @@ app.use("/api/v1/products", productRouter)
 
 //route for order
 app.use("/api/v1/orders", orderRouter)
+
+
+//route for cart 
+app.use("/api/v1/carts", cartRouter)
 
 export default app;
